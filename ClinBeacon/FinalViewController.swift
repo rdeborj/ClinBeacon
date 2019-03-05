de9424e7814bb498b079ee5da0068e1dab1ade44
Richard de Borja <richard.deborja@uhnresearch.ca>
2019-03-05 08:24:52 -0500

    added a simple test swipe gesture

---
+++ ClinBeacon/FinalViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FinalViewController.swift
+//  ClinBeacon
+//
+//  Created by Richard de Borja on 2019-03-05.
+//  Copyright © 2019 The University Health Network. All rights reserved.
+//
+
+import Foundation
