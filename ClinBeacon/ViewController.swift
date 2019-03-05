5e042fa38353ce39d22b1f4613fa8df73328d273
Richard de Borja <richard.deborja@uhnresearch.ca>
2019-03-05 07:28:52 -0500

    Initial Commit

---
+++ ClinBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClinBeacon
+//
+//  Created by Richard de Borja on 2019-03-05.
+//  Copyright © 2019 The University Health Network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
